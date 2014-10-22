45ae0501b654870ee76ba4a96c91fd5ad4c4ec48
hibri <hibri@hibri.net>
2014-10-22 16:27:42 +0200

    add gem

---
+++ lib/audit_trail.rb
@@ -0,0 +1,40 @@
+require 'state_machine'
+
+module StateMachine::Auditable
+
+  def self.setup
+    puts 'set'
+    StateMachine::Machine.send(:include, StateMachine::Auditable::AuditTrail)
+  end
+
+end
+
+
+module StateMachine::Auditable::AuditTrail
+
+  def setup_audit_trail(transition_class)
+    @association = transition_class.to_s.tableize.split('/').last.to_sym
+    owner_class.has_many(@association, :class_name => transition_class.to_s) unless owner_class.reflect_on_association(@association)
+    self.after_transition do |object, transition|
+      log(object, transition)
+    end
+  end
+
+  def log(object, transition)
+    # Let ActiveRecord manage the timestamp for us so it does the
+    # right thing with regards to timezones.
+    params = {:event => transition.event, :from => transition.from, :to => transition.to}
+    puts @association
+    if object.new_record?
+      d = object.send(@association).build(params)
+
+    else
+      d =object.send(@association).create(params)
+    end
+    puts d.event
+    nil
+  end
+end
+
+
+StateMachine::Auditable.setup
---
+++ lib/audit_trail/version.rb
@@ -0,0 +1,3 @@
+module AuditTrail
+  VERSION = "0.0.1"
+end
